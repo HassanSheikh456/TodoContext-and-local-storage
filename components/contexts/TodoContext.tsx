38a22ef0bb5c10dce117d5a3c31788678399cfeb
HassanSheikh456 <hassan.nadeem.4012@gmail.com>
2026-03-29 13:23:15 +0500

    A simple and scalable global state setup using React Context API. Provides todo data and actions through a custom hook, making state management clean, reusable, and easy to access across components.Uses createContext, useContext, and a provider pattern to keep state logic clean and reusable.

--- components/contexts/TodoContext.tsx
+++ components/contexts/TodoContext.tsx
@@ -1,8 +1,21 @@
 import { useContext, createContext } from "react";
 
-
-export const TodoContext = createContext({});
+export const TodoContext = createContext({
+  todos: [
+    {
+      id: 1,
+      todo: " Todo msg",
+      completed: false,
+    },
+  ],
+  addTodo: (todo: string) => {},
+  updatedTodo: (id: number, todo: string) => {},
+  deleteTodo: (id: number) => {},
+  toggleComplete: (id: number) => {},
+});
 
 export const useTodo = () => {
-    return useContext(TodoContext)
-}
+  return useContext(TodoContext);
+};
+
+export const TodoProvider = TodoContext.Provider;
